5f662f556665532c3af7b4d3c948dfb6d7b14e9a
Sean Craig <seancraig21@gmail.com>
2020-12-20 17:24:30 -0600

    Added Blinky
    Sprite, Animation, Script and Game Object.
    Waypoints don't fully work yet

---
+++ Assets/GhostMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour
+{
+    public Transform[] waypoints;
+    int cur = 0;
+
+    public float speed = 0.3f;
+
+    void FixedUpdate()
+    {
+        // Waypoint not reached yet? then move closer
+        if (transform.position != waypoints[cur].position)
+        {
+            // Calculate a position that is a bit closer to the waypoint
+            Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+            // Set ghosts position
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        // Waypoint reached, select next one
+        // NOT WORKING!!!!
+        else
+        {
+            //cur = (cur + 1) % waypoints.Length;
+            cur = cur + 1;
+            if (cur == waypoints.Length)
+            {
+                cur = 0;
+            }
+        }
+
+        //Animation
+        Vector2 dir = waypoints[cur].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "pacman")
+        {
+            Destroy(gameObject);
+            // Decease lives, then either respawn or gameover screen
+        }
+    }
+}
